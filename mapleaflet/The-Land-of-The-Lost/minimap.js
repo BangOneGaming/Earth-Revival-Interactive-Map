da5e5451e2499407ef5aaf1699464bd96ac9f72c
BangOneGaming <square.spon@gmail.com>
2025-10-02 16:11:06 +0700

    Create minimap.js

---
+++ mapleaflet/The-Land-of-The-Lost/minimap.js
@@ -0,0 +1,22 @@
+const minimapKeys = {
+    "greenhouse": {
+        "key": "greenhouse",
+        "name": "Greenhouse Overlay",
+        "loc_position": `${59.130562941820834 + 0.12 - 0.01 + 0.01},${-68.85301399018001 + 0.03 - 0.01 - 0.01}`,
+        "bounds": [
+            [59.130562941820834 + 0.12 - 0.30 - 0.01 + 0.01, -68.85301399018001 + 0.03 - 0.583 - 0.01 - 0.01], // SW
+            [59.130562941820834 + 0.12 + 0.30 - 0.01 + 0.01, -68.85301399018001 + 0.03 + 0.583 - 0.01 - 0.01]  // NE
+        ],
+        "main": {
+            "loc_position": `${59.130562941820834 + 0.12 - 0.01 + 0.01},${-68.85301399018001 + 0.03 - 0.01 - 0.01}`,
+            "map_url": "Statics/ER_Mars/statics/greenhouse.webp"
+        },
+        "type": {
+            "default": {
+                "key": "default",
+                "name": "Tutup Greenhouse",
+                "map_url": "Statics/ER_Mars/statics/greenhouse.webp"
+            }
+        }
+    }
+};
